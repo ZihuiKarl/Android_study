9cdbac901e940d0667f4b3f23ec90c23d2f5bbea
ZihuiKarl <yzh813501655@hotmail.com>
2018-08-10 14:04:01 -0600

    Add init files

---
+++ app/src/main/java/com/example/zihuiyang/myapplication/CustomLogic.java
@@ -0,0 +1,4 @@
+package com.example.zihuiyang.myapplication;
+
+public class CustomLogic {
+}
---
+++ app/src/main/java/com/example/zihuiyang/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.zihuiyang.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static java.lang.System.out;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
