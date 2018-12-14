c69566a7b3150dd3e46ac43e18b85930c0bda14d
ZihuiKarl <karlyanggp@gmail.com>
2018-12-13 22:45:35 -0800

    Save and restore UI state in MainActicity.java
    Save and restore ui state using on SaceInstanceState

--- app/src/main/java/com/example/zihuiyang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zihuiyang/myapplication/MainActivity.java
@@ -9,16 +9,28 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    static final String listValue = "listValue";
+    String[] values = new String[4];
+    //= new String[]{"Unit conversion", "Testing Layout", "Saving data", "Search"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null){
+            values = savedInstanceState.getStringArray(listValue);
+        }else{
+            values[0] = "Unit conversion";
+            values[1] = "Testing Layout";
+            values[2] = "Saving data";
+            values[3] = "Search";
+        }
+
         setContentView(R.layout.activity_main);
 
         final ListView listViewApp = findViewById(R.id.listviewapp);
-        String[] values = new String[]{"Unit conversion", "Testing Layout", "Saving data", "Search"};
         final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, values);
         listViewApp.setAdapter(adapter);
 
@@ -46,5 +58,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //Android doc - The activity lifecycle
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putStringArray(listValue,values);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+
+        values = savedInstanceState.getStringArray(listValue);
     }
 }
